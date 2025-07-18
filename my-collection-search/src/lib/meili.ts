b3b24b40d58e0e976f093350c688e3c02a266941
Adam Saegebarth <adam.saegebarth@gmail.com>
2025-07-17 18:55:16 -0700

    update envs for build

--- my-collection-search/src/lib/meili.ts
+++ my-collection-search/src/lib/meili.ts
@@ -9,10 +9,6 @@ export function getMeiliClient({ server = false } = {}) {
     ? process.env.MEILISEARCH_API_KEY
     : process.env.NEXT_PUBLIC_MEILISEARCH_API_KEY || process.env.MEILISEARCH_API_KEY;
 
-  if (!host) {
-    throw new Error("Missing MeiliSearch host environment variable");
-  }
-
   return new MeiliSearch({ host, apiKey });
 }
 
