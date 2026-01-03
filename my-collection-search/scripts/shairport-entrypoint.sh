321a962c9608038441ed2433c571405a603ed406
Adam Saegebarth <adam.saegebarth@gmail.com>
2026-01-02 16:25:03 -0800

    fix error

--- my-collection-search/scripts/shairport-entrypoint.sh
+++ my-collection-search/scripts/shairport-entrypoint.sh
@@ -2,8 +2,8 @@
 # Shairport-Sync Entrypoint Script
 # Processes config template with environment variables before starting
 
-# Process the config template with envsubst
-envsubst < /etc/shairport-sync.conf.template > /etc/shairport-sync.conf
+# Process the config template with sed (more portable than envsubst)
+sed "s|\${AUDIO_DEVICE}|$AUDIO_DEVICE|g" /etc/shairport-sync.conf.template > /etc/shairport-sync.conf
 
 # Call the original s6-overlay entrypoint
 exec /init
