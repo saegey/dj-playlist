d09d38aa5e4b62a75f7e67168c3b5a0047481b51
Adam Saegebarth <adam.saegebarth@gmail.com>
2026-05-05 08:24:48 -0700

    ios new proj

---
+++ ios/DJPlaylist​Mobile/DJPlaylist​Mobile/DJPlaylist_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  DJPlaylist_MobileApp.swift
+//  DJPlaylist​Mobile
+//
+//  Created by Adam Saegebarth on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct DJPlaylist_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
